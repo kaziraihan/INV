e1d52051fc5bd91983fbfab7be2d772db5a1af87
kaziraihan <63977320+kaziraihan@users.noreply.github.com>
2025-09-03 16:57:06 +0600

    Update percentage_asset_report.php

--- admin/report/percentage_asset_report.php
+++ admin/report/percentage_asset_report.php
@@ -42,19 +42,9 @@
                     dept.cus_address AS department, 
                     a.assettype, 
                     COUNT(a.assettype) AS asset_count,
-                    (COUNT(a.assettype) * 100.0 / 
-                        (SELECT COUNT(*) 
-                        FROM asset_list 
-                        WHERE usedbyid IN (
-                            SELECT id 
-                            FROM customer 
-                            WHERE cus_address = dept.cus_address 
-                            AND status = '0'
-                        )
-                        )
-                    ) AS percentage 
+                    (COUNT(a.assettype) / (SELECT COUNT(*) FROM asset_list WHERE usedbyid IN (SELECT id FROM customer WHERE cus_address = dept.cus_address))) * 100 AS percentage
                 FROM asset_list a
-                INNER JOIN customer dept ON a.usedbyid = dept.id
+                INNER JOIN customer dept ON a.usedbyid = dept.id WHERE dept.status = 1
                 GROUP BY dept.cus_address, a.assettype
                 ORDER BY dept.cus_address, a.assettype;
             ";
